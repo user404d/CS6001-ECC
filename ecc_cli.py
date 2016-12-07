e5cab517d9544743c214cfc5aacf551a5a03ad94
ColinConduff <cconduff098@gmail.com>
2016-12-07 10:37:22 -0600

    Use json for diffie_hellman inputs.

--- ecc_cli.py
+++ ecc_cli.py
@@ -6,61 +6,35 @@ Command line interface for ECC and Diffie Hellman
 User provides two private keys.  ECC and Diffie Hellman are
 used to generate public keys and shared secret keys.
 """
-
+import json
 import os, sys, getopt
 from ecc_impl import ECC
 from diffie_hellman import DiffieHellman
 
 def main(argv):
-   using_example_1 = False # no other input is necessary
+   """
+   Example input_ecc_cli.json 
+   {
+    "point_x": "0x188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012",
+    "point_y": "0x07192b95ffc8da78631011ed6b24cdd573f977a11e794811",
+    "a": "-3",
+    "b": "0x64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1",
+    "p": "6277101735386680763835789423207666416083908700390324961279",
+    "private_key1": "7919",
+    "private_key2": "8387"
+   }
+   """
 
-   # Default arguments 
-   point_x = 0x188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012
-   point_y = 0x07192b95ffc8da78631011ed6b24cdd573f977a11e794811
-   a = -3
-   b = 0x64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1
-   p = 6277101735386680763835789423207666416083908700390324961279
+   with open('input_ecc_cli.json') as data_file:    
+       data = json.load(data_file)
 
-   private_key1 = 7919
-   private_key2 = 8387
-
-   help_text = 'ecc_cli.py --private1 <private key 1> --private2 <private key 2> ' \
-      '\nor run an example with:\n' \
-      'ecc_cli.py --private1 ex --private2 ex'
-
-   if len(argv) == 0:
-      print(help_text)
-      sys.exit(2)
-
-   try:
-      opts, args = getopt.getopt(argv,"h:", ["private1=", "private2="])
-      # opts, args = getopt.getopt(argv,"hx:y:a:b:p:k1:k2:")
-   except getopt.GetoptError:
-      print(help_text)
-      sys.exit(2)
-   
-   # Process input from arguments 
-   for opt, arg in opts:
-      if opt == '-h':
-         print(help_text)
-         sys.exit()
-      # elif opt == ("-example1"):
-      #    using_example_1 = True
-      #    break
-      # elif opt == ("-x"):
-      #    point_x = int(arg)
-      # elif opt == ("-y"):
-      #    point_y = int(arg)
-      # elif opt == ("-a"):
-      #    a = int(arg)
-      # elif opt == ("-b"):
-      #    b = int(arg)
-      # elif opt == ("-p"):
-      #    p = int(arg)
-      elif opt == ("--private1") and arg != "ex":
-         private_key1 = int(arg)
-      elif opt == ("--private2") and arg != "ex":
-         private_key2 = int(arg)
+   point_x = int(data["point_x"], 16)
+   point_y = int(data["point_y"], 16)
+   a = int(data["a"])
+   b = int(data["b"], 16)
+   p = int(data["p"])
+   private_key1 = int(data["private_key1"])
+   private_key2 = int(data["private_key2"])
 
    # Check for valid input
    if private_key1 <= 0 or private_key2 <= 0:
